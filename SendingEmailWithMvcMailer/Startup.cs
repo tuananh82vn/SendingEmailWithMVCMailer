e8ee87f5c71ad98953fe4b1a6bf7b0370ec693a5
tuananh82vn <tuananh82vn@yahoo.com>
2015-06-30 10:51:17 +1000

    First

---
+++ SendingEmailWithMvcMailer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendingEmailWithMvcMailer.Startup))]
+namespace SendingEmailWithMvcMailer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
